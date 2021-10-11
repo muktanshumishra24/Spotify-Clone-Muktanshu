0c08a3fd9178029fcb6218654531b45d90c9eeac
muktanshumishra24 <77049812+muktanshumishra24@users.noreply.github.com>
2021-10-12 02:38:04 +0530

    Implemented Full Front-End(Final Commit)

--- src/reducer.js
+++ src/reducer.js
@@ -11,23 +11,29 @@ export const initialState = {
 const reducer = (state,action) =>{
     console.log(action);
 
-    switch(action.type){
-        case "SET_USER":
-            return{
-              ...state,
-              user:action.user  
-            }
-        case "SET_TOKEN":
-            return{
-                ...state,
-                token:action.token
-            }
-        case "SET_PLAYLISTS":
-            return{
-                ...state,playlists:action.playlists,
-            }
-        default:
-            return state;
+    switch (action.type) {
+      case "SET_USER":
+        return {
+          ...state,
+          user: action.user,
+        };
+      case "SET_TOKEN":
+        return {
+          ...state,
+          token: action.token,
+        };
+      case "SET_PLAYLISTS":
+        return {
+          ...state,
+          playlists: action.playlists,
+        };
+      case "SET_DISCOVER_WEEKLY":
+        return {
+          ...state,
+          discover_weekly: action.discover_weekly,
+        };
+      default:
+        return state;
     }
 }
 
